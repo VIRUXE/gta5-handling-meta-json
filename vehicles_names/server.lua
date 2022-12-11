e563c3d39577ec451a0d211952cc87f8cea31a1e
VIRUXE <flavioaspereira@gmail.com>
2022-12-11 18:10:03 +0000

    rm vehicle_names resource - useless

--- vehicles_names/server.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent('vehicleNames:send', function (vehicleNames)
-    print('Saving vehicle names to file...')
-    SaveResourceFile(GetCurrentResourceName(), 'vehicle_names.json', json.encode(vehicleNames), -1)
-end)
